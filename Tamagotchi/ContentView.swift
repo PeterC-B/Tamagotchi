f922a286ebd5e3db3649f74d0968314cc24f8df8
Peter Compton-Burnett <compton-burnett.p@etoncollege.org.uk>
2022-01-26 11:21:11 +0000

    Lots of work

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -8,32 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var tamagotchi = Tamagotchi()
+    @State private var counter = 0
+    @State var currentDate = Date()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var aliveTimer = 0
+    
+
     var body: some View {
         VStack{
             Circle()
                 .fill(Color.blue)
                 .frame(minWidth: 50, idealWidth: 75, maxWidth: 200, minHeight: 75, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Section{
+                Text("Hunger: \(tamagotchi.hunger)")
+                Text("Happiness: \(tamagotchi.happiness)")
+                Text("Age: \(tamagotchi.age) years old")
+                Text("Height: \(tamagotchi.height)cm")
+                Text("Hygiene: \(tamagotchi.hygiene) points")
+                Text("Health: \(tamagotchi.health) points")
+                Text("Counter: \(counter)")
+            }
             Form{
                 Section{
-                    Button("Feed") {}
+                    Button("Eat Meal") {
+                        tamagotchi.eatMeal()
+                    }
                         .padding()
                         .border(Color.blue, width: 1)
                         .cornerRadius(10)
-                    Button("Feed") {}
+                    Button("Eat Snack") {
+                        tamagotchi.eatSnack()
+                    }
                         .padding()
                         .border(Color.blue, width: 1)
                         .cornerRadius(10)
                 }
                 Section{
-                    Button("Feed") {}
+                    Button("Play Game") {
+                        tamagotchi.playGame()
+                    }
                         .padding()
                         .border(Color.blue, width: 1)
                         .cornerRadius(10)
-                    Button("Feed") {}
+                    Button("Clean Up") {
+                        tamagotchi.cleanUp()
+                    }
                         .padding()
                         .border(Color.blue, width: 1)
                         .cornerRadius(10)
                 }
+                
+                Text("Tamagotchi Status:")
+                Text(tamagotchi.dead == true ? "Dead": "Alive")
+                    .alert(isPresented: $tamagotchi.dead) {
+                        Alert(title: Text("Tamagotchi Died"),
+                              dismissButton: .destructive(Text("Play Again...")) {
+                                                  tamagotchi = Tamagotchi()
+                                              }
+                        )
+                    }
+                 
+                Text("\(currentDate)")
+                            .onReceive(timer) { input in
+                                currentDate = input
+                                aliveTimer += 1
+                                if aliveTimer % 3 == 0{
+                                    //tamagotchi.hungerTimer()
+                                }
+                                if aliveTimer % 10 == 0{
+                                    //tamagotchi.ageIncrement()
+                                }
+                                if aliveTimer % 2 == 0{
+                                    if Int.random(in: 1...2) == 1{
+                                        tamagotchi.makeSick()
+                                    }
+                                }
+                            }
                 
                 
                 
--- Tamagotchi/Tamagotchi.swift
+++ Tamagotchi/Tamagotchi.swift
@@ -7,37 +7,65 @@
 
 import Foundation
 
-class Tamagotchi{
-    var hunger: Int = 5
-    var happiness: Int = 10
-    var age: Int = 0
-    var height: Int = 40
-    var hygiene: Int = 3
+class Tamagotchi: ObservableObject{
+    @Published var hunger: Int = 5
+    @Published var happiness: Int = 10
+    @Published var age: Int = 0
+    @Published var height: Int = 40
+    @Published var hygiene: Int = 3
+    @Published var health: Int = 10
+    @Published var dead = false
     
     func eatSnack(){
         if hunger > 0{
             hunger -= 1
         }
+        checkDeath()
         
     }
     
     func eatMeal() {
         hunger = 0
+        checkDeath()
     }
     
     func playGame() {
         hunger += 3
         happiness += 10
+        checkDeath()
     }
     
     func growUp() {
         age += 1
         height += 8
+        checkDeath()
     }
     
     func cleanUp() {
         hygiene += 4
         happiness += 2
+        checkDeath()
+    }
+    
+    func checkDeath() {
+        if hunger >= 30 || hygiene == 0 || age == 10 || health <= 0{
+            dead = true
+        }
+    }
+    
+    func hungerTimer() {
+        hunger += 5
+        checkDeath()
+    }
+    
+    func ageIncrement() {
+        age += 1
+        checkDeath()
+    }
+    
+    func makeSick() {
+        health -= 5
+        checkDeath()
     }
     
 }
